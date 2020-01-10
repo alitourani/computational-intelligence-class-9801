b9fe5a8cb2f48749d117e9062a63054d7f90fdad
محمد علي الأشقر <57432065+mhdalialashkar@users.noreply.github.com>
2020-01-10 10:53:29 -0800

    ويرايش 4م
     جهش  كروموزوم و درست كردن خطا مسير تا بهترين كروموزوم به دست بياد

--- 12-genetic-chromosome-collision/p.java
+++ 12-genetic-chromosome-collision/p.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.logging.Level;
@@ -19,13 +17,14 @@ import javax.swing.Timer;
         //lebal 123=new lable("123");
        int x=0,y=40*11,q,c,v,count_paint;
        boolean hit=false;
+       boolean hdaf=false;
        Timer t=new Timer(1, this);
        Random ran=new Random(); 
        ArrayList Chromosome = new ArrayList();
        ArrayList x_blocks =new ArrayList();
        ArrayList y_blocks = new ArrayList();
-    //ArrayList xx = new ArrayList();
-    //ArrayList yy = new ArrayList();     
+       ArrayList xx = new ArrayList();
+       ArrayList yy = new ArrayList();     
    public void paintComponent(Graphics d){
        if(count_paint != 0){  
            try {
@@ -52,25 +51,50 @@ import javax.swing.Timer;
                    d.fillRect((int)x_blocks.get(n), (int)y_blocks.get(n), 40, 40);
                    
                }
-               if(hit == true){ d.setColor(Color.red); t.stop(); System.err.println(Chromosome);}else{d.setColor(Color.green);}
+               if(hdaf==true){t.stop();}
+               if(hit == true){ 
+                   System.err.println(Chromosome);
+                   System.err.println(xx);
+                   System.err.println(yy);
+                  Chromosome.remove(Chromosome.size()-1);
+                //  Chromosome.remove(Chromosome.size()-2);
+                  xx.remove(xx.size()-1);
+                 // xx.remove(xx.size()-2);
+                  yy.remove(yy.size()-1);
+                 // yy.remove(yy.size()-2);
+                 
+                  x=(int)xx.get(xx.size()-1);
+                  y=(int)yy.get(yy.size()-1);
+                 
+                  hit=false;
+                  //t.stop();
+                     d.setColor(Color.red); 
+                      }else{
+                   System.out.println(Chromosome);
+                   d.setColor(Color.green);
+                   xx.add(x);
+                   yy.add(y);
+               }
+               
                d.fillRect(x,y, 40,40);
-               System.out.println(Chromosome);
+               
               
              }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(x==40*11 && y==0){hdaf=true;}
+        if(x>40*11 || y<0){hit=true;}
+        for(int g=0; g<x_blocks.size(); g++){
+        if(x == (int)x_blocks.get(g) && y == (int)y_blocks.get(g)){
+            hit=true;
+         } }
+        if(hit==false){
         q=ran.nextInt(2);
         Chromosome.add(q);
-        for(int g=0; g<x_blocks.size(); g++){
-        if(x == (int)x_blocks.get(g) && y == (int)y_blocks.get(g)){hit=true;}
-        }
         if(q == 1){y-=40;}
         if(q == 0){x+=40;}
-        count_paint++;   repaint();   
-       // if(count_paint ==20){repaint(); }
-    }
-   
-
-   
+        }
+        count_paint++;   repaint();
+    }  
 }
